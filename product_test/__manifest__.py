630b2e59bc0a7b0b717807f1e7aa25f8262f0436
jcarlosmontoya <jcarlosmontoyach@gmail.com>
2019-02-19 18:35:56 +0100

    Add test module

---
+++ product_test/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Test",
+
+    'summary': """
+        Add new field test product template""",
+
+    'description': """
+        Product test
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['product'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    "application": True,
+    # only loaded in demonstration mode
+}
